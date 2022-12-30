6ec8742f64e9ea435b3932611191f1da4699f215
drobot-k <kr.drobot@gmail.com>
2022-12-30 12:32:32 +0500

    done module 6

--- app/components/report-form.js
+++ app/components/report-form.js
@@ -1,22 +1,66 @@
+/* eslint-disable no-unused-vars */
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { get, set, computed } from '@ember/object';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default Component.extend({
+const Validations = buildValidations({
+  bookRating: [
+    validator('ds-error'),
+    validator('presence', true),
+    // validator('number'),
+  ],
+  presentationURL: [
+    validator('ds-error'),
+    validator('presence', true),
+    validator('format', { type: 'url' })
+  ],
+  videoURL: [
+    validator('ds-error'),
+    validator('presence', true),
+    validator('format', { type: 'url' })
+  ],
+  book: [
+    validator('ds-error'),
+    validator('presence', true),
+  ],
+  speaker: [
+    validator('ds-error'),
+    validator('presence', true),
+  ],
+  review: [
+    validator('ds-error'),
+    validator('presence', true),
+  ],
+});
+
+export default Component.extend(Validations, {
+  isFormValid: computed.alias('validations.isValid'),
+  error: false,
   store: service(),
+
     actions: {
         submitForm(e) {
           e.preventDefault();
+
+          if (this.get('isFormValid')) {
+            this.set('error', false)
     
-          this.onsubmit({
-            reportDate: this.get('date'),
-            // meetingId: this.get('meetingId'),
-            book: this.get('book'),
-            speaker: this.get('speaker'),
-            bookRating: this.get('bookRating'),
-            presentationURL: this.get('presentationURL'),
-            videoURL: this.get('videoURL'),
-            review: this.get('review'),
-          });
+            this.onsubmit({
+              reportDate: this.get('date'),
+              // meetingId: this.get('meetingId'),
+              book: this.get('book'),
+              speaker: this.get('speaker'),
+              bookRating: this.get('bookRating'),
+              presentationURL: this.get('presentationURL'),
+              videoURL: this.get('videoURL'),
+              review: this.get('review'),
+            });
+          }
+
+          else {
+            this.set('error', true)
+          }
         },
 
         searchBook(query) {
@@ -27,8 +71,8 @@ export default Component.extend({
           return this.get('store').query('speaker', { q: query })
         },
 
-        async clear(report) {          
-          await report.destroyRecord();
+        clear(report) {          
+          report.destroyRecord();
           this.get('store').unloadRecord(report);
       },
       },
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,15 +1,36 @@
 import Controller from "@ember/controller";
 import { inject as service } from '@ember/service';
+import { set, get, computed } from '@ember/object'
+import ENV from 'homework/config/environment';
 
 export default Controller.extend({
   session: service(),
   currentUser: service(),
+  i18n: service(),
+
+  locale: ENV.i18n.defaultLocale,
+  langRu: computed('locale', function() {
+    return get(this, 'locale') === 'ru';
+  }),
+  langEn: computed('locale', function() {
+    return get(this, 'locale') === 'en';
+  }),
 
   actions: {
     async logout(e) {
       e.preventDefault();
 
       this.get('session').invalidate();
+    },
+
+    changeLang (lang) {
+      set(this, 'locale', lang.target.value);
+      set(this, 'i18n.locale', get(this, 'locale'));
     }
-  }
+  },
+
+  // init() {
+  //   this._super(...arguments);
+  //   set(this, 'i18n.locale', 'en');
+  // }
 });
